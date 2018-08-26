16aa6f72c129ab4f791c6debd9a64508e034ffb5
Binjam Gebre <info@react-entwickler.de>
2018-08-27 00:28:52 +0200

    chart verbessert(2)

--- src/Chart.js
+++ src/Chart.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 //import * as V from 'victory';
-import {VictoryChart, VictoryZoomContainer, VictoryLine, VictoryBrushContainer, VictoryAxis} from 'victory';
+import {VictoryChart, VictoryZoomContainer, VictoryLine, VictoryBrushContainer, VictoryTheme} from 'victory';
+
+
+import './css.css';
 
 
 class Chart extends Component {
@@ -21,7 +24,10 @@ class Chart extends Component {
     render() {
       return (
         <div>
-            <VictoryChart width={1100} height={350} scale={{x: "time"}}
+            <VictoryChart 
+            theme={VictoryTheme.material}
+            padding={{top: 10, left: 50, right: 50, bottom: 30}}
+            width={1100} height={350} scale={{x: "time"}}
               containerComponent={
                 <VictoryZoomContainer responsive={false}
                   zoomDimension="x"
@@ -30,32 +36,34 @@ class Chart extends Component {
                 />
               }
             >
-              <VictoryLine
+              <VictoryLine 
                 style={{
-                  data: {stroke: "green"}
+                  data: {
+                    stroke: "green",
+                    strokeWidth: 5
+                  },
+                  
                 }}
                 data={[
-                  {x: new Date(2018, 8, 1,  1, 55, 20).toLocaleDateString(), y:  5 + "°C"},
-                  {x: new Date(2018, 8, 2,  6, 55, 20).toLocaleDateString(), y:  7 + "°C"},
-                  {x: new Date(2018, 8, 3, 10, 55, 20).toLocaleDateString(), y: 12 + "°C"},
-                  {x: new Date(2018, 8, 4, 12, 55, 20).toLocaleDateString(), y: 13 + "°C"},
-                  {x: new Date(2018, 8, 5, 14, 55, 20).toLocaleDateString(), y: 15 + "°C"},
-                  {x: new Date(2018, 8, 6, 16, 55, 20).toLocaleDateString(), y: 21 + "°C"},
-                  {x: new Date(2018, 8, 7, 18, 55, 20).toLocaleDateString(), y: 22 + "°C"},
-                  {x: new Date(2018, 8, 8, 20, 55, 20).toLocaleDateString(), y: 28 + "°C"},
-                  {x: new Date(2018, 8, 9, 21, 10, 20).toLocaleDateString(), y: 34 + "°C"},
-                  {x: new Date(2018, 8, 10, 22, 30, 20).toLocaleDateString(), y: 37 + "°C"},
-                  {x: new Date(2018, 8, 11, 22, 40, 20).toLocaleDateString(), y: 42 + "°C"},
-                  {x: new Date(2018, 8, 12, 23, 55, 20).toLocaleDateString(), y: 44 + "°C"}
-                  
+                  {x: new Date(2018, 8,  1,  1, 55, 20).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}), y:  5 + "°C"},
+                  {x: new Date(2018, 8,  2,  6, 55, 20).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}), y:  7 + "°C"},
+                  {x: new Date(2018, 8,  3, 10, 55, 20).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}), y:  3 + "°C"},
+                  {x: new Date(2018, 8,  4, 10, 55, 20).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}), y:  3 + "°C"},
+                  {x: new Date(2018, 8,  4, 12, 55, 20).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}), y:  8 + "°C"},
+                  {x: new Date(2018, 8,  5, 12, 55, 20).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}), y:  8 + "°C"},
+                  {x: new Date(2018, 8,  5, 13, 55, 20).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}), y:  5 + "°C"},
+                  {x: new Date(2018, 8,  6, 14, 55, 20).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}), y:  20 + "°C"},
+                
                 ]}
               />
   
             </VictoryChart>
+
   
             <VictoryChart
-              padding={{top: 0, left: 50, right: 50, bottom: 30}}
-              width={1100} height={350} scale={{x: "time"}}
+              theme={VictoryTheme.material}          
+              padding={{top: 10, left: 50, right: 50, bottom: 30}}
+              width={880} height={450} scale={{x: "time"}}
               containerComponent={
                 <VictoryBrushContainer responsive={false}
                   brushDimension="x"
@@ -64,37 +72,40 @@ class Chart extends Component {
                 />
               }
             >
-              <VictoryAxis
+              {/* <VictoryAxis
                 tickValues={[
                   new Date(2018, 8, 1),
-                  new Date(2018, 9, 1),
-
-
-                  /* new Date(1990, 1, 1),
-                  new Date(1995, 1, 1),
-                  new Date(2000, 1, 1),
-                  new Date(2005, 1, 1),
-                  new Date(2010, 1, 1) */
+                  new Date(2018, 8, 2),
+                  new Date(2018, 8, 3),
+                  new Date(2018, 8, 4),
+                  new Date(2018, 8, 5),
+                  new Date(2018, 8, 6),
+                  new Date(2018, 8, 7),
+                  new Date(2018, 8, 8),
+                  new Date(2018, 8, 9),
+                  new Date(2018, 8, 10),
+                  new Date(2018, 8, 11),
+                  new Date(2018, 8, 12),
                 ]}
-               // tickFormat={(x) => new Date(x).toLocaleDateString()}
-              />
+                tickFormat={(x) => new Date(x).setUTCFullYear([], {month:"string"})}
+              />  */}
               <VictoryLine
                 style={{
                   data: {stroke: "red"}
                 }}
                 data={[
-                  {x: new Date(2018, 8, 1,  1, 55, 20).toLocaleTimeString(), y:  5 + "°C"},
-                  {x: new Date(2018, 8, 2,  6, 55, 20).toLocaleTimeString(), y:  7 + "°C"},
-                  {x: new Date(2018, 8, 3, 10, 55, 20).toLocaleTimeString(), y: 12 + "°C"},
-                  {x: new Date(2018, 8, 4, 12, 55, 20).toLocaleTimeString(), y: 13 + "°C"},
-                  {x: new Date(2018, 8, 5, 14, 55, 20).toLocaleTimeString(), y: 15 + "°C"},
-                  {x: new Date(2018, 8, 6, 16, 55, 20).toLocaleTimeString(), y: 21 + "°C"},
-                  {x: new Date(2018, 8, 7, 18, 55, 20).toLocaleTimeString(), y: 22 + "°C"},
-                  {x: new Date(2018, 8, 8, 20, 55, 20).toLocaleTimeString(), y: 28 + "°C"},
-                  {x: new Date(2018, 8, 9, 21, 10, 20).toLocaleTimeString(), y: 34 + "°C"},
-                  {x: new Date(2018, 8, 10, 22, 30, 20).toLocaleTimeString(), y: 37 + "°C"},
-                  {x: new Date(2018, 8, 11, 22, 40, 20).toLocaleTimeString(), y: 42 + "°C"},
-                  {x: new Date(2018, 8, 12, 23, 55, 20).toLocaleTimeString(), y: 44 + "°C"}
+
+                  {x: new Date(2018, 8,  1,  1, 55, 20).toLocaleTimeString(), y:  5 + "°C"},
+                  {x: new Date(2018, 8,  2,  6, 55, 20).toLocaleTimeString(), y:  7 + "°C"},
+                  {x: new Date(2018, 8,  3, 10, 55, 20).toLocaleTimeString(), y:  3 + "°C"},
+                  {x: new Date(2018, 8,  4, 10, 55, 20).toLocaleTimeString(), y:  3 + "°C"},
+                  {x: new Date(2018, 8,  4, 12, 55, 20).toLocaleTimeString(), y:  8 + "°C"},
+                  {x: new Date(2018, 8,  5, 12, 55, 20).toLocaleTimeString(), y:  8 + "°C"},
+                  {x: new Date(2018, 8,  5, 13, 55, 20).toLocaleTimeString(), y:  5 + "°C"},
+                  {x: new Date(2018, 8,  6, 14, 55, 20).toLocaleTimeString(), y:  20 + "°C"},
+
+
+                  
                 ]}
 
               />
